d91b3aee374ede808b520b386439e6df24e782fa
t3k0o <t3k0olg@gmail.com>
2019-08-11 21:23:58 -0500

    se agrego las secuencias

--- juego.js
+++ juego.js
@@ -1,19 +1,31 @@
 const celeste = document.getElementById('celeste')
-      const violeta = document.getElementById('violeta')
-      const naranja = document.getElementById('naranja')
-      const verde = document.getElementById('verde')
-      const btnEmpezar = document.getElementById('btnEmpezar')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+const btnEmpezar = document.getElementById('btnEmpezar')
 
-      class Juego {
-        constructor() {
-          this.inicializar()
+class Juego {
+    constructor() {
+        this.inicializar()
+        this.generarSecuencia()
+    }
+
+    inicializar() {
+        btnEmpezar.classList.add('hide')
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
         }
+    }
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map( n => Math.floor( Math.random() * 4 ))
+        
+    }
+}
 
-        inicializar() {
-          btnEmpezar.classList.add('hide')
-        }
-      }
-
-      function empezarJuego() {
-        var juego = new Juego()
-      }
+function empezarJuego() {
+   window.juego = new Juego()
+}
